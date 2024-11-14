59ae45ffdd74f46792a29dbf8e8604af89d7b842
pradeepnnv <pradeepnnv@gmail.com>
2024-11-14 00:00:02 -0600

    Add status field

--- internal/todo/todo.go
+++ internal/todo/todo.go
@@ -2,26 +2,33 @@ package todo
 
 import "errors"
 
+type Item struct {
+	Task   string
+	Status string
+}
 type Service struct {
-	todos []string
+	todos []Item
 }
 
 func NewService() *Service {
 	return &Service{
-		todos: make([]string, 0),
+		todos: make([]Item, 0),
 	}
 }
 
 func (svc *Service) Add(todo string) error {
 	for _, t := range svc.todos {
-		if t == todo {
+		if t.Task == todo {
 			return errors.New("todo is not unique")
 		}
 	}
-	svc.todos = append(svc.todos, todo)
+	svc.todos = append(svc.todos, Item{
+		Task:   todo,
+		Status: "TO_BE_STARTED",
+	})
 	return nil
 }
 
-func (svc *Service) GetAll() []string {
+func (svc *Service) GetAll() []Item {
 	return svc.todos
 }
